de40ce06c0d5b763e236483aa8e5f5bd0f857af5
TimaSv-Uk <sviridenkotimofei@gmail.com>
2025-01-01 19:41:16 +0200

    add dfs and methods to make nodes neigbours

--- labyr1.js
+++ labyr1.js
@@ -10,7 +10,6 @@ class GameNode {
 	 * */
 	node_id;
 
-
 	/**
 	 * @param {number} node_id
 	 * */
@@ -18,16 +17,14 @@ class GameNode {
 		this.node_id = node_id;
 	}
 
-
 	/**
 	 * @param {number} neighbour_node_id
 	 * @param {boolean} accessable
 	 * */
 	add_neighbour_node(neighbour_node_id, accessable) {
-		this.neighbour_nodes.add({ neighbour_node_id: neighbour_node_id, accessable: accessable });
+		this.neighbour_nodes.push({ neighbour_node_id: neighbour_node_id, accessable: accessable });
 	}
 }
-
 
 
 
@@ -54,15 +51,15 @@ class GameGrid {
 	 *
 	 * @type {number} 
 	 * */
-	destinations_node_id;
+	destination_node_id;
 
 	/**
 	 *
 	 * @param {Array<GameNode>} nodes
 	 * @param {number} start_node_id
-	 * @param {number} destinations_node_id
+	 * @param {number} destination_node_id
 	 * */
-	constructor(nodes, start_node_id, destinations_node_id) {
+	constructor(nodes, start_node_id, destination_node_id) {
 
 		if (this.is_valid_nodes(nodes)) {
 			this.nodes = nodes
@@ -71,12 +68,12 @@ class GameGrid {
 		if (!nodes.find((node) => node.node_id === start_node_id)) {
 			throw new Error("there is no node with currentNodeId in nodes");
 		}
-		if (!nodes.find((node) => node.node_id === destinations_node_id)) {
+		if (!nodes.find((node) => node.node_id === destination_node_id)) {
 			throw new Error("there is no node with destinationNodeId in nodes");
 		}
 		this.start_node_id = start_node_id;
 		this.current_node_id = start_node_id;
-		this.destinations_node_id = destinations_node_id;
+		this.destination_node_id = destination_node_id;
 
 	}
 	/**
@@ -108,7 +105,100 @@ class GameGrid {
 		return true;
 	}
 
+
+
+	/**
+	 * Find a node by its ID.
+	 * @param {number} node_id
+	 * @returns {GameNode | undefined}
+	 */
+	find_node(node_id) {
+		return this.nodes.find(node => node.node_id === node_id);
+	}
+
+	/**
+	 * @param {number} node_1_id
+	 * @param {number} node_2_id
+	 * @param {boolean} accessable
+	 * */
+	make_neighbour_to_node1(node_1_id, node_2_id, accessable) {
+
+		let node_1 = this.find_node(node_1_id);
+		let node_2 = this.find_node(node_2_id);
+
+		if (node_1_id === node_2_id) {
+			throw new Error(`Same nodes can not be neigbours`);
+		}
+		if (!node_1) {
+			throw new Error(`there is no node_1 with ID: ${node_1_id}`);
+		};
+		if (!node_2) {
+			throw new Error(`there is no node_2 with ID: ${node_2_id}`);
+		}
+		node_1.add_neighbour_node(node_2_id, accessable);
+		// this.neighbour_nodes.add({ neighbour_node_id: neighbour_node_id, accessable: accessable });
+	}
+	/**
+	 * @param {number} node_1_id
+	 * @param {number} node_2_id
+	 * @param {boolean} accessable
+	 * */
+	make_neighbour_nodes_same_accessablity(node_1_id, node_2_id, accessable) {
+
+		let node_1 = this.find_node(node_1_id);
+		let node_2 = this.find_node(node_2_id);
+
+		if (node_1_id === node_2_id) {
+			throw new Error(`Same nodes can not be neigbours`);
+		}
+		if (!node_1) {
+			throw new Error(`there is no node_1 with ID: ${node_1_id}`);
+		};
+		if (!node_2) {
+			throw new Error(`there is no node_2 with ID: ${node_2_id}`);
+		}
+		node_1.add_neighbour_node(node_2_id, accessable);
+		node_2.add_neighbour_node(node_1_id, accessable);
+		// this.neighbour_nodes.add({ neighbour_node_id: neighbour_node_id, accessable: accessable });
+	}
+
+
+	/**
+	 * @returns {boolean}
+	 */
+	can_reach_destination() {
+
+		const visited = new Set();
+
+		const dfs = (current_node_id) => {
+
+			if (visited.has(current_node_id)) {
+				return false;
+			}
+
+			visited.add(current_node_id);
+
+			let current_node = this.find_node(current_node_id);
+
+			if (!current_node) {
+				return false;
+			}
+
+			if (current_node_id === this.destination_node_id) {
+
+				console.log("found", current_node_id);
+				return true;
+			}
+
+			return current_node.neighbour_nodes.some((neighbour) => {
+				return neighbour.accessable && dfs(neighbour.neighbour_node_id)
+			});
+		};
+
+		return dfs(this.start_node_id);
+	}
 }
+
 /**
  * @param {GameGrid} game 
  * @param {string} element_to_append 
@@ -122,8 +212,7 @@ function render_nodes(game, element_to_append = "body") {
 	let cols = Math.floor(Math.sqrt(nodes.length));
 	grid.style.gridTemplateColumns = `repeat(${cols}, auto)`;
 
-
-	nodes.sort((node1, node2) => node1.node_id - node2.node_id).forEach((nodeData, id) => {
+	nodes.sort((node1, node2) => node1.node_id - node2.node_id).forEach((nodeData) => {
 		let html_node = document.createElement("div");
 		html_node.classList.add("node");
 		html_node.id = nodeData.node_id;
@@ -134,7 +223,7 @@ function render_nodes(game, element_to_append = "body") {
 
 			html_node.innerText = "Start";
 		}
-		if (nodeData.node_id === game.destinations_node_id) {
+		if (nodeData.node_id === game.destination_node_id) {
 			html_node.classList.add("destination");
 			html_node.innerText = "Destination";
 		}
@@ -143,7 +232,7 @@ function render_nodes(game, element_to_append = "body") {
 	});
 
 	document.querySelector(element_to_append).appendChild(grid);
-	return
+	return;
 }
 
 
@@ -152,11 +241,8 @@ function select_walls_start(node_selector = ".node", max_closed_walls = 10) {
 	let nodes = document.querySelectorAll(node_selector); nodes.forEach((node) => {
 		node.addEventListener('click', (event) => {
 
-
 			const element = event.target;
-
 			const style = getComputedStyle(element);
-			const borderWidth = parseInt(style.borderWidth);
 			const offsetX = event.offsetX;
 			const offsetY = event.offsetY;
 			const innerWidth = element.clientWidth;
@@ -203,18 +289,72 @@ function select_walls_start(node_selector = ".node", max_closed_walls = 10) {
 		});
 	});
 }
+
+/**
+ * @param {GameGrid} game 
+ * @param {string} element_to_append 
+ * */
+function render_nodes(game, element_to_append = "body") {
+	let nodes = game.nodes;
+	const grid = document.createElement("div");
+	grid.id = "grid";
+
+	// Calculate the number of columns
+	let cols = Math.floor(Math.sqrt(nodes.length));
+	grid.style.gridTemplateColumns = `repeat(${cols}, auto)`;
+
+	nodes.sort((node1, node2) => node1.node_id - node2.node_id).forEach((nodeData) => {
+
+		let html_node = document.createElement("div");
+
+		html_node.classList.add("node");
+		html_node.id = nodeData.node_id;
+
+		nodeData.neighbour_nodes.forEach((neighbour) => {
+
+			let neighbour_path = document.createElement("div");
+
+			neighbour_path.innerText = `${neighbour.neighbour_node_id} -- ${neighbour.accessable}`
+			neighbour_path.classList.add("neighbour_path");
+			html_node.appendChild(neighbour_path);
+		})
+
+		if (nodeData.node_id === game.start_node_id) {
+			html_node.classList.add("current");
+			html_node.classList.add("start");
+
+			// html_node.innerText = "Start";
+		}
+		if (nodeData.node_id === game.destination_node_id) {
+			html_node.classList.add("destination");
+			// html_node.innerText = "Destination";
+		}
+
+		grid.appendChild(html_node);
+	});
+
+	document.querySelector(element_to_append).appendChild(grid);
+	return;
+}
 let nodes = [
-	new GameNode(1, true, false),
-	new GameNode(2, false, false),
-	new GameNode(3, false, false),
-	new GameNode(4, false, false),
-	new GameNode(5, false, false),
-	new GameNode(6, false, false),
-	new GameNode(7, false, false),
-	new GameNode(8, false, false),
-	new GameNode(9, false, true),
+	new GameNode(1),
+	new GameNode(2),
+	new GameNode(3),
+	new GameNode(4),
+
+	// new GameNode(5),
+	// new GameNode(6),
+	// new GameNode(7),
+	// new GameNode(8),
+	// new GameNode(9),
 ];
-let game = new GameGrid(nodes, 1, 9);
+let game = new GameGrid(nodes, 1, 4);
+
+game.make_neighbour_nodes_same_accessablity(1, 2, true);
+game.make_neighbour_nodes_same_accessablity(2, 3, true);
+game.make_neighbour_nodes_same_accessablity(3, 4, true);
+
+console.log(game.can_reach_destination());
 
 render_nodes(game, "body");
 
