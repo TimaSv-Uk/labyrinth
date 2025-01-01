76260f3ee0804f95541efdf9e29ba2407d54c64a
TimaSv-Uk <sviridenkotimofei@gmail.com>
2025-01-01 20:57:24 +0200

    render only right and bottom borders exept on edges

--- labyr1.js
+++ labyr1.js
@@ -211,6 +211,14 @@ function render_nodes(game, element_to_append = "body") {
 	// Calculate the number of columns
 	let cols = Math.floor(Math.sqrt(nodes.length));
 	grid.style.gridTemplateColumns = `repeat(${cols}, auto)`;
+	document.styleSheets.appendChild
+	// .node:nth-child(${cols}n) {
+	//   border-right: none;
+	// }
+	//
+	// .node:nth-last-child(-n + ${cols}) {
+	//   border-bottom: none;
+	// }
 
 	nodes.sort((node1, node2) => node1.node_id - node2.node_id).forEach((nodeData) => {
 		let html_node = document.createElement("div");
@@ -236,9 +244,83 @@ function render_nodes(game, element_to_append = "body") {
 }
 
 
+
+/**
+ * @param {GameGrid} game 
+ * @param {string} element_to_append 
+ * */
+function render_nodes(game, element_to_append = "body") {
+	let nodes = game.nodes;
+	const grid = document.createElement("div");
+	grid.id = "grid";
+
+	// Calculate the number of columns
+	let cols = Math.floor(Math.sqrt(nodes.length));
+
+	grid.style.gridTemplateColumns = `repeat(${cols}, minmax(0, 1fr))`;
+	//remove borders from edges
+	let styleText = document.getElementById('style').sheet;
+	let borderRightRule = `.node:nth-child(${cols}n) { border-right: none; }`;  // Last element in each row
+	let borderBottomRule = `.node:nth-last-child(-n + ${cols}) { border-bottom: none; }`;  // Last row
+	styleText.insertRule(borderRightRule, styleText.cssRules.length);
+	styleText.insertRule(borderBottomRule, styleText.cssRules.length);
+
+	nodes.sort((node1, node2) => node1.node_id - node2.node_id).forEach((nodeData) => {
+
+		let html_node = document.createElement("div");
+
+		html_node.classList.add("node");
+		html_node.id = nodeData.node_id;
+		html_node.innerText = `NodeId: ${nodeData.node_id};`;
+		nodeData.neighbour_nodes.forEach((neighbour) => {
+
+			let neighbour_path = document.createElement("div");
+
+			neighbour_path.innerText = `Path to: ${neighbour.neighbour_node_id} -- ${neighbour.accessable};`
+			neighbour_path.classList.add("neighbour_path");
+			html_node.appendChild(neighbour_path);
+		})
+
+		if (nodeData.node_id === game.start_node_id) {
+			html_node.classList.add("current");
+			html_node.classList.add("start");
+
+			// html_node.innerText = "Start";
+		}
+		if (nodeData.node_id === game.destination_node_id) {
+			html_node.classList.add("destination");
+			// html_node.innerText = "Destination";
+		}
+
+		grid.appendChild(html_node);
+	});
+
+	document.querySelector(element_to_append).appendChild(grid);
+	return;
+}
+
+/**
+ * 
+ * @param {Element} element 
+ * @param {("Top"|"Right"|"Left"|"Bottom")} border_direction 
+ */
+function toggle_node_border(element, border_direction) {
+
+	selectedBorder = "border" + border_direction + "Style";
+	if (style[selectedBorder] === 'solid' && max_closed_walls > 0) {
+		// element.style[selectedBorder] = 'hidden';
+		element.style[selectedBorder] = 'dashed';
+		max_closed_walls -= 1;
+	}
+	else if (style[selectedBorder] === 'dashed') {
+		element.style[selectedBorder] = 'solid';
+		max_closed_walls += 1;
+	}
+}
 function select_walls_start(node_selector = ".node", max_closed_walls = 10) {
 
-	let nodes = document.querySelectorAll(node_selector); nodes.forEach((node) => {
+	let nodes = document.querySelectorAll(node_selector);
+	nodes.forEach((node) => {
 		node.addEventListener('click', (event) => {
 
 			const element = event.target;
@@ -275,7 +357,7 @@ function select_walls_start(node_selector = ".node", max_closed_walls = 10) {
 
 			selectedBorder = "border" + selectedDirection + "Style";
 
-
+			
 			if (style[selectedBorder] === 'solid' && max_closed_walls > 0) {
 				// element.style[selectedBorder] = 'hidden';
 				element.style[selectedBorder] = 'dashed';
@@ -289,53 +371,6 @@ function select_walls_start(node_selector = ".node", max_closed_walls = 10) {
 		});
 	});
 }
-
-/**
- * @param {GameGrid} game 
- * @param {string} element_to_append 
- * */
-function render_nodes(game, element_to_append = "body") {
-	let nodes = game.nodes;
-	const grid = document.createElement("div");
-	grid.id = "grid";
-
-	// Calculate the number of columns
-	let cols = Math.floor(Math.sqrt(nodes.length));
-	grid.style.gridTemplateColumns = `repeat(${cols}, auto)`;
-
-	nodes.sort((node1, node2) => node1.node_id - node2.node_id).forEach((nodeData) => {
-
-		let html_node = document.createElement("div");
-
-		html_node.classList.add("node");
-		html_node.id = nodeData.node_id;
-
-		nodeData.neighbour_nodes.forEach((neighbour) => {
-
-			let neighbour_path = document.createElement("div");
-
-			neighbour_path.innerText = `${neighbour.neighbour_node_id} -- ${neighbour.accessable}`
-			neighbour_path.classList.add("neighbour_path");
-			html_node.appendChild(neighbour_path);
-		})
-
-		if (nodeData.node_id === game.start_node_id) {
-			html_node.classList.add("current");
-			html_node.classList.add("start");
-
-			// html_node.innerText = "Start";
-		}
-		if (nodeData.node_id === game.destination_node_id) {
-			html_node.classList.add("destination");
-			// html_node.innerText = "Destination";
-		}
-
-		grid.appendChild(html_node);
-	});
-
-	document.querySelector(element_to_append).appendChild(grid);
-	return;
-}
 let nodes = [
 	new GameNode(1),
 	new GameNode(2),
@@ -347,11 +382,20 @@ let nodes = [
 	// new GameNode(7),
 	// new GameNode(8),
 	// new GameNode(9),
+	//
+	// new GameNode(10),
+	// new GameNode(11),
+	// new GameNode(12),
+	// new GameNode(13),
+	// new GameNode(14),
+	// new GameNode(15),
+	// new GameNode(16),
 ];
 let game = new GameGrid(nodes, 1, 4);
 
 game.make_neighbour_nodes_same_accessablity(1, 2, true);
-game.make_neighbour_nodes_same_accessablity(2, 3, true);
+game.make_neighbour_nodes_same_accessablity(1, 3, true);
+game.make_neighbour_nodes_same_accessablity(2, 4, true);
 game.make_neighbour_nodes_same_accessablity(3, 4, true);
 
 console.log(game.can_reach_destination());
